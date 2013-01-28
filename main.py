91cc72df66cdb8e726e2e5b9a89c50396378ba21
sabmek <michael.karpeles@gmail.com>
2013-01-28 10:51:23 +0000

    Added ?p page GET param for nav bar + form url negotiation

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import waltz
-from waltz import render, track
+from waltz import web, render, track
 
 urls = ('/?', 'Home')
 sessions = {"uid": None,
             "uname": "",
             "logged": False}
+env = {"type": type}
 app = waltz.setup.dancefloor(urls, globals(), sessions=sessions,
-                             autoreload=False)
+                             env=env, autoreload=False)
 
 class Home:
     @track
     def GET(self):
-        return render().index()
+        i = web.input(p=0)
+        return render().index(page=int(i.p))
 
 if __name__ == "__main__":
     app.run()
