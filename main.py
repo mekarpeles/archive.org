a386a6fb46289d03c737e625584d879090b76145
sabmek <michael.karpeles@gmail.com>
2013-01-30 06:07:56 +0000

    Added a different render view for serp pages. searches from serp page stay on serp

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import waltz
 from waltz import web, render, track
+from urlparse import urlparse
+import requests
 
 urls = ('/?', 'Home')
 sessions = {"uid": None,
             "uname": "",
             "logged": False}
-env = {"type": type}
+env = {'commify': web.commify}
 app = waltz.setup.dancefloor(urls, globals(), sessions=sessions,
                              env=env, autoreload=False)
 
 class Home:
     @track
     def GET(self):
-        i = web.input(p=0)
-        return render().index(page=int(i.p))
+        i = web.input(p='0')
+        return render().index(p=int(i.p))
+
+    def POST(self):
+        i = web.input(q="", p='0', rows=50, page=1)
+        serp = "<p>No Results Found</p>"
+        zeroclick=""
+        if i.p == '0' and 'http://' in i.q:
+            zeroclick = '<img style="height: 50px;" src="http://archive.org/images/wayback.gif"/><span style="position: relative; top: -14px; margin-left: 10px;">Go Wayback in time! <a style="margin-left: 10px;" href="http://web.archive.org/web/*/%s">View older versions of %s</a><span>' % (i.q, i.q)
+            #raise web.seeother('http://web.archive.org/web/*/%s' % i.q)
+
+        # Fallback to seach all mediatypes
+        if i.q:
+            r = requests.post('http://archive.org/advancedsearch.php',
+                              data={'q': i.q.replace('http://', ''),
+                                    'rows': i.rows,
+                                    'page': i.page, 'output': 'json'})
+            serp = r.json
+        return render().serp(query=i.q, p=i.p, serp=serp,
+                             page=i.page, rows=i.rows,
+                             zeroclick=zeroclick)
 
 if __name__ == "__main__":
     app.run()
