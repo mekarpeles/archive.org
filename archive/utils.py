389135738159ffc01e41d1c00b1e286bbe69c929
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-06-21 20:24:50 -0700

    migrated main.py logic for env to utils

--- archive/utils.py
+++ archive/utils.py
@@ -1,6 +1,14 @@
+#-*-coding: utf-8 -*-
+
+"""
+    utils
+    ~~~~~
+    
+    General project independent utilities
+"""
+
 import re
-import urllib
-import markdown
+import markdown as md
 
 URL_PATTERN = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z0-9.\-]{0,3}|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
  
@@ -16,6 +24,6 @@ def join(x, y):
 def truncate(s, length):
     return '%s ...' % s[:length] if len(s) > length else s
 
-def markdown(safe_mode=True, replace_with=''):
-    return markdown.Markdown(safe_mode=safe_mode,
-                             html_replacement_text=replace_with).convert
+def markdown(html, safe_mode=True, replace_with=''):
+    return md.Markdown(safe_mode=safe_mode,
+                       html_replacement_text=replace_with).convert(html)
